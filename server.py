09ac374ec080dcea14637947bf4bb6fb8e41b731
Rona Wang <rona@mit.edu>
2024-02-25 15:04:55 -0800

    test

--- server.py
+++ server.py
@@ -4,21 +4,23 @@ from nbconvert.preprocessors import ExecutePreprocessor
 
 app = Flask(__name__)
 
-@app.route('/get_notebook_output')
+
+@app.route("/get_notebook_output")
 def get_notebook_output():
     # Load the notebook
-    with open('master_notebook.ipynb') as f:
+    with open("./Deepfake-detection/master_notebook.ipynb") as f:
         nb = nbformat.read(f, as_version=4)
 
     # Execute the notebook and capture the output
-    ep = ExecutePreprocessor(timeout=2000, kernel_name='python3')
+    ep = ExecutePreprocessor(timeout=2000, kernel_name="python3")
     ep.preprocess(nb)
 
     # Extract the output from the last cell
-    output = nb.cells[-1].outputs[0].data['text/plain']
+    output = nb.cells[-1].outputs[0].data["text/plain"]
 
     # Return the output as a JSON response
     return jsonify(output=output)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
